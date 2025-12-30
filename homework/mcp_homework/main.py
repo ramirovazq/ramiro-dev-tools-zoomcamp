c4f1aead26ec0723e4c1c0bef50f315d98181489
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-30 10:48:21 -0600

    Add simple fastmcp in main

--- homework/mcp_homework/main.py
+++ homework/mcp_homework/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from mcp-homework!")
+from fastmcp import FastMCP
 
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
